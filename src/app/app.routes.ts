07a4a2633c38ceaaedda0c9d6338883c3bbaa519
kush6 <kush60513@gmail.com>
2025-03-10 21:39:56 +0530

    change

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,5 +5,10 @@ export const routes: Routes = [
     {
         path:'home',
         component:HomeComponent
+    },
+    {
+        path:'',
+        redirectTo:'/home',
+        pathMatch:'full'
     }
 ];
