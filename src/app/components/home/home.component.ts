45bf2df672232f2c00dc3438c820f7425a4698d5
kush6 <kush60513@gmail.com>
2025-03-10 21:37:37 +0530

    change

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { TypeWriiterComponent } from "../type-wriiter/type-wriiter.component";
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-home',
-  imports: [TypeWriiterComponent],
+  imports: [TypeWriiterComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
