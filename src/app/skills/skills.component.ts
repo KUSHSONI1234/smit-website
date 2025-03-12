bd02b4473d801092486d35f15fcfc640d343b75b
kush6 <kush60513@gmail.com>
2025-03-12 21:39:40 +0530

    changes

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -4,9 +4,30 @@ import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-skills',
-  imports: [FormsModule,CommonModule],
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.css'
 })
 export class SkillsComponent {
+  skills = [
+    { name: 'C++', icon: 'devicon-cplusplus-plain' },
+    { name: 'C', icon: 'devicon-cplusplus-plain' },
+    { name: 'HTML', icon: 'devicon-html5-plain' },
+    { name: 'CSS', icon: 'devicon-css3-plain' },
+    { name: 'JavaScript', icon: 'devicon-javascript-plain' },
+    { name: 'Node.js', icon: 'devicon-nodejs-plain' },
+    { name: 'React.js', icon: 'devicon-react-original' },
+    { name: 'MongoDB', icon: 'devicon-mongodb-plain' },
+    { name: 'Express.js', icon: 'devicon-express-original' },
+    { name: 'Angular', icon: 'devicon-angular-plain' }
+  ];
+
+  visibleSkills = 5;
+  isExpanded = false;
+
+  toggleView() {
+    this.isExpanded = !this.isExpanded;
+    this.visibleSkills = this.isExpanded ? this.skills.length : 5;
+  }
 }
