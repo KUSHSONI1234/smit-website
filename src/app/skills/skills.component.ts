6f2e672fc460ebd68cba5601af1e332598ea02c8
kush6 <kush60513@gmail.com>
2025-03-12 22:12:08 +0530

    changes

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { FooterComponent } from "../components/footer/footer.component";
 
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
   styleUrls: ['./skills.component.css'],
-  imports:[FormsModule,CommonModule]
+  imports: [FormsModule, CommonModule, FooterComponent]
 })
 export class SkillsComponent {
   skills = [
