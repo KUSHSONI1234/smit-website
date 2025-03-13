f3bb58416ae73f8f448be461bfb8941863a45d62
kush6 <kush60513@gmail.com>
2025-03-13 22:21:01 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./components/navbar/navbar.component";
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { PreloaderComponent } from "./components/preloader/preloader.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, CommonModule, FormsModule, PreloaderComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'portfolio-website';
+  isLoading = true;
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 3000); // Adjust the duration
+  }
 }
