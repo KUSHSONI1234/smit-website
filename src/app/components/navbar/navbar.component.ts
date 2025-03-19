8dbd3a6b2ba70f1ee71b2092247dcae246bf6f0b
kush6 <kush60513@gmail.com>
2025-03-19 23:06:29 +0530

    changes

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  standalone: true,
+  imports: [RouterLink, FormsModule, CommonModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-  
+    isNavbarOpen = false;
+
+    toggleNavbar(): void {
+        this.isNavbarOpen = !this.isNavbarOpen;
+    }
+
+    closeNavbar(): void {
+        this.isNavbarOpen = false;
+    }
 }
